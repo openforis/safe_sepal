b9ce9b81a554ba19fd6cf6efb982597ed215cdf9
lecrabe <remi.dannunzio@gmail.com>
2019-06-26 16:06:54 +0200

    modif

--- scripts/master.R
+++ scripts/master.R
@@ -1,4 +1,4 @@
 scriptdir  <- paste0(path.expand("~"),"/safe_sepal/scripts/")
-
+setwd(scriptdir)
 source(paste0(scriptdir,"s0_parameters.R"))
-source(paste0(scriptdir,"s0_parameters.R"))
+source(paste0(scriptdir,"s1_setworking.R"))
